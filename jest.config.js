ce8b64242ed8353737feff2af39608645871b622
DevNickjr <nicholasduadei14@gmail.com>
2024-07-07 16:05:21 +0100

    configure tests

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+};
+
+// "jest": {
+//   "moduleFileExtensions": [
+//     "js",
+//     "json",
+//     "ts"
+//   ],
+//   "rootDir": "src",
+//   "testRegex": ".*\\.spec\\.ts$",
+//   "transform": {
+//     "^.+\\.(t|j)s$": "ts-jest"
+//   },
+//   "collectCoverageFrom": [
+//     "**/*.(t|j)s"
+//   ],
+//   "coverageDirectory": "../coverage",
+//   "testEnvironment": "node"
+// },
+
+// {
+//   "moduleFileExtensions": ["js", "json", "ts"],
+//   "rootDir": ".",
+//   "testEnvironment": "node",
+//   "testRegex": ".e2e-spec.ts$",
+//   "transform": {
+//     "^.+\\.(t|j)s$": "ts-jest"
+//   }
+// }
